b5064251efa0afab326dcd6a221ca86fb746208e
Jan Klimke <jan.klimke@3dcontentlogistics.com>
2025-01-30 14:57:32 +0100

    removed remaining debug output

--- lib/jsonapi/swagger/json.rb
+++ lib/jsonapi/swagger/json.rb
@@ -1,7 +1,6 @@
 module Jsonapi
   module Swagger
     class Json
-
       attr_accessor :path
 
       def initialize(path = 'swagger/v1/swagger.json')
@@ -9,7 +8,11 @@ module Jsonapi
       end
 
       def parse_doc
-        @doc ||= JSON.parse(load) rescue Hash.new{ |h, k| h[k]= {} }
+        @doc ||= begin
+                   JSON.parse(load)
+                 rescue StandardError
+                   Hash.new { |h, k| h[k] = {} }
+                 end
       end
 
       def base_path
@@ -18,14 +21,12 @@ module Jsonapi
 
       def load
         @data ||= if File.exist?(path)
-                  IO.read(path)
-                else
-                  puts "create swagger.json in #{path}"
-                  '{}'
-                end
+                    IO.read(path)
+                  else
+                    puts "create swagger.json in #{path}"
+                    '{}'
+                  end
       end
-
-
     end
   end
 end
